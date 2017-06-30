a993eb1147dc1b3f2f9c1cdac756d5ba30690dff
ArpitKumarSrivastava <arpityuuvraaj@gmail.com>
2017-06-30 21:21:03 +0530

    Add files via upload

---
+++ Models/Contacts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneBook.Models
+{
+    public class Contacts
+    {
+        public int PersonId { get; set; }
+        public string Name { get; set; }        
+        public string Contact { get; set; }        
+    }
+}
---
+++ Models/SMSLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneBook.Models
+{
+
+    public class SMSLog
+    {
+        public string Name { get; set; }
+        public string Contact { get; set; }
+        public string Timestamp { get; set; }
+    }
+
+}
